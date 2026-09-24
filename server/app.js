2c042d4d1a4b832d615f016fd60901696c806b90
Chaitanya Sai <chaitanyasai768@gmail.com>
2026-09-24 13:58:14 +0530

    aa

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 dotenv.config()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://lmschaitanya.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true
 }));
